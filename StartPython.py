e07d8eb0f090910ca3a39073f8e2106b3e50d470
Kingshuk Dash <kingshukdash@yahoo.com>
2018-05-19 19:14:58 -0700

    Add files via upload
    To check what version of software plug-ins are installed

---
+++ StartPython.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 18:30:47 2017
+
+@author: KingDash
+"""
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+import flask
+print('flask: {}'.format(flask.__version__))
+#import fuzzywuzzy
+#print('fuzzywuzzy: {}'.format(fuzzywuzzy.__version__))
+import difflib
